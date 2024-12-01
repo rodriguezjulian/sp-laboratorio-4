4ba5e8611dde5974b519b82e3c2d0a5889197fbb
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-12-01 03:34:52 -0300

    descargas gral + directiva para btn excel general

--- clinica-online/src/app/components/seccion-usuarios/seccion-usuarios.component.ts
+++ clinica-online/src/app/components/seccion-usuarios/seccion-usuarios.component.ts
@@ -3,12 +3,15 @@ import { Router } from '@angular/router';
 import { FirestoreService } from '../../servicios/firestore.service';
 import { CommonModule } from '@angular/common';
 import { trigger, transition, style, animate } from '@angular/animations';
+import * as XLSX from 'xlsx';
+import {pasaPorArriba} from '../../directivas/app-tooltip-directive.directive'
+import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-seccion-usuarios',
   templateUrl: './seccion-usuarios.component.html',
   styleUrls: ['./seccion-usuarios.component.scss'],
-  imports : [CommonModule],
+  imports : [CommonModule,pasaPorArriba],
   standalone : true,
   animations: [
     trigger('fadeInUp', [
@@ -66,5 +69,59 @@ export class SeccionUsuariosComponent {
     } catch (error) {
       console.error('Error al cargar los usuarios:', error);
     }
+  }
+
+  exportarUsuariosExcel() {
+    const data: any[] = [];
+
+    // Pacientes
+    data.push(['Pacientes']);
+    data.push(['Nombre', 'Apellido', 'Correo', 'DNI', 'Edad', 'Obra Social']);
+    this.pacientes.forEach((paciente) => {
+      data.push([
+        paciente.nombre,
+        paciente.apellido,
+        paciente.correo,
+        paciente.dni,
+        paciente.edad,
+        paciente.obraSocial || 'N/A',
+      ]);
+    });
+
+    data.push([]);
+    // Especialistas
+    data.push(['Especialistas']);
+    data.push(['Nombre', 'Apellido', 'Correo', 'DNI', 'Edad', 'Especialidades', 'Habilitado']);
+    this.especialistas.forEach((especialista) => {
+      data.push([
+        especialista.nombre,
+        especialista.apellido,
+        especialista.correo,
+        especialista.dni,
+        especialista.edad,
+        especialista.especialidades,
+        especialista.habilitado ? 'Sí' : 'No',
+      ]);
+    });
+
+    data.push([]);
+    // Administradores
+    data.push(['Administradores']);
+    data.push(['Nombre', 'Apellido', 'Correo', 'DNI', 'Edad']);
+    this.administradores.forEach((admin) => {
+      data.push([
+        admin.nombre,
+        admin.apellido,
+        admin.correo,
+        admin.dni,
+        admin.edad,
+      ]);
+    });
+
+    // Generar el archivo Excel
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Usuarios');
+    XLSX.writeFile(wb, 'usuarios.xlsx');
   }
 }
