b061eaf3eec4c29b6dd8f14dc34dbe1c66c6ccea
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-12-01 19:14:12 -0300

    update

--- clinica-online/src/app/components/ver-historia-clinica/ver-historia-clinica.component.ts
+++ clinica-online/src/app/components/ver-historia-clinica/ver-historia-clinica.component.ts
@@ -1,25 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-ver-historia-clinica',
   templateUrl: './ver-historia-clinica.component.html',
   styleUrls: ['./ver-historia-clinica.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports : [CommonModule]
 })
 export class VerHistoriaClinicaComponent {
   @Input() paciente: any; // Paciente con su historia clínica
   @Output() cerrarModal = new EventEmitter<void>();
 
-  constructor(private cdr: ChangeDetectorRef) {}
-
   async ngOnInit() {
-    console.log('Desde ver historia clínica', this.paciente?.historiaClinica);
-    this.cdr.detectChanges(); // Forzar la detección de cambios
-  }
 
+    console.log("Desde ver historia clinica " ,  this.paciente);
+  }
   cerrar() {
-    this.cerrarModal.emit();
+      this.cerrarModal.emit();
   }
 }
