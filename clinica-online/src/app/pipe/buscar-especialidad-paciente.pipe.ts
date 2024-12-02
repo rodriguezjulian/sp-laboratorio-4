fbb60b7d20998a3c90cc5fbcc43ac7c62c489547
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-12-02 02:20:13 -0300

    pipe para mis turnos asignados completa

--- clinica-online/src/app/pipe/buscar-especialidad-paciente.pipe.ts
+++ clinica-online/src/app/pipe/buscar-especialidad-paciente.pipe.ts
@@ -5,14 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true, // Convertido a standalone
 })
 export class BuscarPacienteEspecialidadPipe implements PipeTransform {
+  
   transform(turnos: any[], buscar: string): any[] {
     if (!buscar) return turnos;
     const lowerBuscar = buscar.toLowerCase();
 
     return turnos.filter((turno) => {
       // Filtrar por nombre o especialidad
+      console.log("desde pipe " ,  turnos);
       const matchesNombre = turno.nombre.toLowerCase().includes(lowerBuscar);
       const matchesEspecialidad = turno.especialidad.toLowerCase().includes(lowerBuscar);
+
+
+      const matchAltura = turno.historiaClinica?.altura.toLowerCase().includes(lowerBuscar);
+      const peso  = turno.historiaClinica?.peso.toLowerCase().includes(lowerBuscar);
+      const presion  = turno.historiaClinica?.presion.toLowerCase().includes(lowerBuscar);
+      const temperatura  = turno.historiaClinica?.temperatura.toLowerCase().includes(lowerBuscar);
+
 
       // Filtrar por datosDinamicos
       const matchesDatosDinamicos = turno.historiaClinica?.datosDinamicos?.some((dato: any) => 
@@ -20,7 +29,7 @@ export class BuscarPacienteEspecialidadPipe implements PipeTransform {
         dato.valor.toLowerCase().includes(lowerBuscar)
       );
 
-      return matchesNombre || matchesEspecialidad || matchesDatosDinamicos;
+      return matchesNombre || matchesEspecialidad || matchesDatosDinamicos || matchAltura || peso || presion || temperatura;
     });
   }
 }
