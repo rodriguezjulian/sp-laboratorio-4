de3b1a0691574fa66351672a3460df1416fc3fd9
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-12-03 03:02:05 -0300

    host

---
+++ clinica-online/public/chunk-7FG5ZD4D.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./chunk-SLHCDPS3.js";var a=class t{loading=!1;constructor(){}setLoader(o){o?this.loading=o:setTimeout(()=>{this.loading=o},500)}getLoader(){return this.loading}static \u0275fac=function(r){return new(r||t)};static \u0275prov=e({token:t,factory:t.\u0275fac,providedIn:"root"})};export{a};
